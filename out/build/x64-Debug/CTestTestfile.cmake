c4a24dec0077fff53ab23ff299ad8e4cf2fa86a8
Кирилл <zoolohol@mail.ru>
2020-04-11 10:18:55 +0300

    deleted some files

---
+++ out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/zilot/source/repos/zilottar/lab-06-multithreads
+# Build directory: C:/Users/zilot/source/repos/zilottar/lab-06-multithreads/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit_tests "C:/Users/zilot/source/repos/zilottar/lab-06-multithreads/out/build/x64-Debug/tests.exe")
+set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/zilot/source/repos/zilottar/lab-06-multithreads/CMakeLists.txt;52;add_test;C:/Users/zilot/source/repos/zilottar/lab-06-multithreads/CMakeLists.txt;0;")
