6d586979cb2cbf8140d1181dabe33e9438124f06
Кирилл <zoolohol@mail.ru>
2020-04-11 09:56:44 +0300

    Update source.cpp

--- sources/source.cpp
+++ sources/source.cpp
@@ -1,3 +1,12 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <header.hpp>
+#include <logs.h>
+#include <thread>
+int main(int argc, char *argv[]) {
+    unsigned int m = std::thread::hardware_concurrency();
+    if ((argc > 1) && (atoi(argv[1])))
+        m = atoi(argv[1]);
+    header::startTreads(m);
+    return 0;
+}
