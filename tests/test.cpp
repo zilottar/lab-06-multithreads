98acab8bb0230dcc8a0860a46c8013049befb21c
Кирилл <zoolohol@mail.ru>
2020-05-31 08:33:00 +0300

    2

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,6 +1,16 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <gtest/gtest.h>
+#include <header.hpp>
+#include <logs.h>
+#include <thread>
+int main(int argc, char *argv[]) {
+    unsigned int m = std::thread::hardware_concurrency();
+    if ((argc > 1) && (atoi(argv[1])))
+        m = atoi(argv[1]);
+    header::startTreads(m);
+    return 0;
+}
 
 TEST(Example, EmptyTest) {
     EXPECT_TRUE(true);
