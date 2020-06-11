48fe2d8507c71d89e8e0982f6955a70159586502
Кирилл <zoolohol@mail.ru>
2020-06-11 10:09:40 +0300

    1

--- include/header.h
+++ include/header.h
@@ -2,38 +2,91 @@
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
-#include <picosha2.h>
-#include <regex>
-#include <string>
+
 #include <iostream>
+#include "picosha2.h"
 #include <thread>
-#include <logs.h>
-#include <boost/thread.hpp>
+#include <vector>
+#include <boost/log/trivial.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/sources/severity_feature.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/console.hpp>
+#include <boost/log/expressions.hpp>
 
-namespace header {
-    void tryregex(std::string s) {
-        std::string hash_hex_str;
-        picosha2::hash256_hex_string(s.begin(), s.end(), hash_hex_str);
-        logs::logTrace(s, hash_hex_str);
-        if (std::regex_match(hash_hex_str, std::regex {R"(\w{60}+[0]{4})"})) {
-            logs::logInfo(s, hash_hex_str);
+namespace logging = boost::log;
+namespace keywords = boost::log::keywords;
+
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::vector;
+
+
+class hashMaster {
+public:
+    uint64_t threadNumber;
+    vector <std::thread> allThreads;
+    
+    hashMaster() {
+        threadNumber = std::thread::hardware_concurrency();
+        setThreads();
+    }
+
+    explicit hashMaster(int number) {
+        threadNumber = number;
+        setThreads();
+    }
+
+
+
+    void setThreads() {
+        for (uint64_t i = 0; i < threadNumber; i++) {
+            std::thread th(hashHandler, i, this);
+            allThreads.push_back(std::move(th));
+        }
+        for (uint64_t i = 0; i < threadNumber; i++) {
+            allThreads.at(i).join();
         }
     }
 
-    void runner(int random){
+
+
+    static void hashHandler(int a, hashMaster *obj) {
+        obj->init_logging();
         while (true){
-            static unsigned int rand = time(nullptr)+random;
-            unsigned int a = rand_r(&rand)%10000;
-            tryregex(std::to_string(a));
+            int stringLenth = 30;
+            std::thread::id threadID=obj->allThreads.at(a).get_id();
+            string stringForHash;
+            for (int i = 0; i < stringLenth; ++i) {
+                stringForHash.push_back(std::rand()%255);
+            }
+            const std::string hash = picosha2::hash256_hex_string(stringForHash);
+            if (hash.find("0000", 60) == 60) {
+                
+                BOOST_LOG_TRIVIAL(info) << stringForHash << " - " << hash << " - " << threadID << endl;
+                break;
+            }
+            else
+                BOOST_LOG_TRIVIAL(trace) << stringForHash << " - " << " - " << hash << " - " << threadID << endl;
         }
     }
 
-    void startTreads(unsigned int m){
-        logs::init();
-        boost::thread_group threads;
-        for (unsigned int i = 0; i < m; ++i)
-            threads.create_thread(boost::bind(runner, boost::cref(i)));
-        threads.join_all();
+    static void init_logging() {
+        
+        logging::add_file_log (
+                        keywords::file_name = "info.log",
+                        keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%"
+                );
+
+        logging::add_console_log (
+                std::cout,
+                keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%"
+        );
+
+        logging::add_common_attributes();
     }
-}// namespace header
-#endif // INCLUDE_HEADER_HPP_
+};
+
